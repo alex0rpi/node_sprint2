05cc26a6b7ec2398e58d74b886014673602d2ebb
alex0rpi <orpi.alex@hotmail.com>
2023-02-08 16:48:52 +0100

    2.3 spotify to be reviewed

--- mongoDB-estructura/N2E1-youtube.js
+++ mongoDB-estructura/N2E1-youtube.js
@@ -1,19 +1,19 @@
 use('YOUTUBE');
 
-// db.videos.insertMany([
-//   { _id: ObjectId('0000000000000000000000c1'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
-//   likes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b1')}],
-//   dislikes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b2')}],
-//   estat: ''},
-//   { _id: ObjectId('0000000000000000000000c2'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
-//   likes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b2')}],
-//   dislikes: [],
-//   estat: ''},
-//   { _id: ObjectId('0000000000000000000000c3'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
-//   likes: [],
-//   dislikes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b1')}],
-//   estat: ''},
-// ]);
+db.videos.insertMany([
+  { _id: ObjectId('0000000000000000000000c1'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
+  likes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b1')}],
+  dislikes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b2')}],
+  estat: ''},
+  { _id: ObjectId('0000000000000000000000c2'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
+  likes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b2')}],
+  dislikes: [],
+  estat: ''},
+  { _id: ObjectId('0000000000000000000000c3'), titol: '', descripcio: '', data_hora:ISODate(), grandaria: '', nom_arxiu: '', durada: '', thumbnail: '', reproduccions: '',
+  likes: [],
+  dislikes: [{date:ISODate(), _uid:ObjectId('0000000000000000000000b1')}],
+  estat: ''},
+]);
 
 db.usuaris.insertMany([
   { _id: ObjectId('0000000000000000000000b1'), email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
--- mongoDB-estructura/N3E1-spotify.js
+++ mongoDB-estructura/N3E1-spotify.js
@@ -1,9 +1,81 @@
 use('SPOTIFY');
 
 db.artists.insertMany([
-  {},
+  { _id:ObjectId('0000000000000000000000b1'), nom: '', imatge: '',
+    albums: [
+      { _id_album:ObjectId('0000000000000000000000a1'), titol: '', any_pub: '', imatge: '',
+        songs: [
+          { _id:ObjectId('000000000000000000000001'), titol: '', durada: '', reproduccions: 599 },
+          { _id:ObjectId('000000000000000000000002'), titol: '', durada: '', reproduccions: 300 },
+        ],
+      },
+      { _id_album:ObjectId('0000000000000000000000a2'), titol: '', any_pub: '', imatge: '',
+        songs: [
+          { _id:ObjectId('000000000000000000000003'), titol: '', durada: '', reproduccions: 201 },
+          { _id:ObjectId('000000000000000000000004'), titol: '', durada: '', reproduccions: 206 },
+        ],
+      },
+    ],
+  },
+  { _id:ObjectId('0000000000000000000000b2'), nom: '', imatge: '',
+    albums: [
+      { titol: '', any_pub: '', imatge: '',
+        songs: [
+          { _id:ObjectId('000000000000000000000005'), titol: '', durada: '', reproduccions: 999 },
+          { _id:ObjectId('000000000000000000000006'), titol: '', durada: '', reproduccions: 1234 },
+        ],
+      },
+    ],
+  },
 ]);
 
 db.usuaris.insertMany([
-  {},
+  { _id:ObjectId('0000000000000000000000d1'), tipus_user: '', email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
+    playlists:[
+      { owner_id:ObjectId('0000000000000000000000d1'), titol:"", num_cançons:"", data_creacio:"", data_eliminacio:"", tipus_playlist:"",
+      canciones:[
+        {id_song:ObjectId('000000000000000000000001')},
+        {id_song:ObjectId('000000000000000000000002')},
+      ]}
+    ],
+    albums_favorits:[
+      {_id_album:ObjectId('0000000000000000000000a1')}
+    ],
+    subscripcio:{ _id:ObjectId('1111111111111111111111a1'), data_inici:ISODate(), data_renovacio:ISODate(),
+      forma_pagament:{ tipus:"targeta", numero_tarjeta:"", mes_caducitat:"", any_caducitat:"", codi_seguretat:"" },
+      pagaments:[
+        {pay_date:ISODate(), total:NumberDecimal('8.99')},
+        {pay_date:ISODate(), total:NumberDecimal('8.99')},
+        {pay_date:ISODate(), total:NumberDecimal('8.99')}
+      ]
+    }
+  },
+  { _id:ObjectId('0000000000000000000000d2'), tipus_user: '', email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
+    playlists:[
+      { owner_id:ObjectId('0000000000000000000000d2'), titol:"", num_cançons:"", data_creacio:"", tipus_playlist:"",
+      canciones:[
+        {id_song:ObjectId('000000000000000000000001')},
+        {id_song:ObjectId('000000000000000000000002')},
+      ]}
+    ],
+    albums_favorits:[
+      {_id_album:ObjectId('0000000000000000000000a2')}
+    ],
+    subscripcio:{ _id:ObjectId('1111111111111111111111a2'), data_inici:ISODate(), data_renovacio:ISODate(),
+      forma_pagament:{ tipus:"paypal", paypal_id:"", paypal_username:"" },
+      pagaments:[
+        {pay_date:ISODate(), total:NumberDecimal('8.99')},
+        {pay_date:ISODate(), total:NumberDecimal('8.99')},
+      ]
+    }
+  },
+  { _id:ObjectId('0000000000000000000000d3'), tipus_user: '', email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
+    albums_favorits:[
+      {_id_album:ObjectId('0000000000000000000000a2')}
+    ],
+    artistes_seguits:[
+      {_id_artista:ObjectId('0000000000000000000000b1')},
+      {_id_artista:ObjectId('0000000000000000000000b2')},
+    ]
+  },
 ]);
