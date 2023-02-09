6c9ab46510b3f43f7a2db4f2b73174c548fe9f87
alexmies <orpi.alex@gmail.com>
2023-02-09 13:26:20 +0100

    finishing sprint 2

--- mongoDB-estructura/N3E1-spotify.js
+++ mongoDB-estructura/N3E1-spotify.js
@@ -3,26 +3,26 @@ use('SPOTIFY');
 db.artists.insertMany([
   { _id:ObjectId('0000000000000000000000b1'), nom: '', imatge: '',
     albums: [
-      { _id_album:ObjectId('0000000000000000000000a1'), titol: '', any_pub: '', imatge: '',
+      { _id_album:ObjectId('0000000000000000000000a1'), titol: '', any_pub:2013, imatge: '',
         songs: [
-          { _id:ObjectId('000000000000000000000001'), titol: '', durada: '', reproduccions: 599 },
-          { _id:ObjectId('000000000000000000000002'), titol: '', durada: '', reproduccions: 300 },
+          { _id:ObjectId('000000000000000000000001'), titol: '', durada: '', reprod: 599 },
+          { _id:ObjectId('000000000000000000000002'), titol: '', durada: '', reprod: 300 },
         ],
       },
-      { _id_album:ObjectId('0000000000000000000000a2'), titol: '', any_pub: '', imatge: '',
+      { _id_album:ObjectId('0000000000000000000000a2'), titol: '', any_pub:2022, imatge: '',
         songs: [
-          { _id:ObjectId('000000000000000000000003'), titol: '', durada: '', reproduccions: 201 },
-          { _id:ObjectId('000000000000000000000004'), titol: '', durada: '', reproduccions: 206 },
+          { _id:ObjectId('000000000000000000000003'), titol: '', durada: '', reprod: 201 },
+          { _id:ObjectId('000000000000000000000004'), titol: '', durada: '', reprod: 206 },
         ],
       },
     ],
   },
   { _id:ObjectId('0000000000000000000000b2'), nom: '', imatge: '',
     albums: [
-      { titol: '', any_pub: '', imatge: '',
+      { titol: '', any_pub:2003, imatge: '',
         songs: [
-          { _id:ObjectId('000000000000000000000005'), titol: '', durada: '', reproduccions: 999 },
-          { _id:ObjectId('000000000000000000000006'), titol: '', durada: '', reproduccions: 1234 },
+          { _id:ObjectId('000000000000000000000005'), titol: '', durada: '', reprod: 999 },
+          { _id:ObjectId('000000000000000000000006'), titol: '', durada: '', reprod: 1234 },
         ],
       },
     ],
@@ -32,7 +32,7 @@ db.artists.insertMany([
 db.usuaris.insertMany([
   { _id:ObjectId('0000000000000000000000d1'), tipus_user: '', email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
     playlists:[
-      { owner_id:ObjectId('0000000000000000000000d1'), titol:"", num_cançons:"", data_creacio:"", data_eliminacio:"", tipus_playlist:"",
+      { owner_id:ObjectId('0000000000000000000000d1'), titol:"", num_songs:2, data_creacio:"", data_eliminacio:"", tipus_playlist:"",
       canciones:[
         {id_song:ObjectId('000000000000000000000001')},
         {id_song:ObjectId('000000000000000000000002')},
@@ -52,10 +52,9 @@ db.usuaris.insertMany([
   },
   { _id:ObjectId('0000000000000000000000d2'), tipus_user: '', email: '', password: '', nom_usuari: '', data_naixement: '', sexe: '', pais: '', codi_postal: '',
     playlists:[
-      { owner_id:ObjectId('0000000000000000000000d2'), titol:"", num_cançons:"", data_creacio:"", tipus_playlist:"",
+      { owner_id:ObjectId('0000000000000000000000d2'), titol:"", num_songs:2, data_creacio:"", tipus_playlist:"",
       canciones:[
-        {id_song:ObjectId('000000000000000000000001')},
-        {id_song:ObjectId('000000000000000000000002')},
+        {id_song:ObjectId('000000000000000000000006')},
       ]}
     ],
     albums_favorits:[
